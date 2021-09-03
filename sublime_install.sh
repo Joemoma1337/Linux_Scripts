188ce8ed1af6983ea28ceea0a26213e83407995a
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2021-09-03 16:12:27 -0400

    Update sublime_install.sh

--- sublime_install.sh
+++ sublime_install.sh
@@ -11,7 +11,7 @@ do_install() {
   detect_pkgmgr "zypper" && echo "Found Zypper" && zypper_install && return
 }
 
-dpkg_install() {
+apt_install() {
  if [ -x "/usr/bin/apt-get" ];
  then
   wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
