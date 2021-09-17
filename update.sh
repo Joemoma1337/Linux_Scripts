ab472dea9b8eabd635c9929fbd28e1ffc322d79e
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2021-09-17 17:45:26 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-# Ansi color code variables
-r="\e[0;91m"
-b="\e[0;94m"
-b_bg="\e[0;104m${expand_bg}"
-r_bg="\e[0;101m${expand_bg}"
-g_bg="\e[0;102m${expand_bg}"
-g="\e[0;92m"
-white="\e[0;97m"
-u="\e[4m"
-re="\e[0m"
 #check for package manager
 var=$(ls /usr/bin | grep -E "^pacman|^apt|^zypper|^yum|^eopkg" | head -n 1)
 echo "found $var package manager"
