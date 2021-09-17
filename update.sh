3561a7720a5bf7d9be29b6d372d5008b279984dd
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2021-09-17 17:33:13 -0400

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,79 +1,55 @@
 #!/bin/bash
-detect_pkgmgr() {
-  ls /usr/bin | grep -E "^$*"
-}
-
-do_update() {
-  echo "Detecting Package Manger ... "
-
-  detect_pkgmgr "apt-get" && echo "Found Apt" && apt_update && return
-  detect_pkgmgr "yum" && echo "Found Yum" && yum_update && return
-  detect_pkgmgr "pacman" && echo "Found Pacman" && pacman_update && return
-  detect_pkgmgr "zypper" && echo "Found Zypper" && zypper_update && return
-}
-
+# Ansi color code variables
+r="\e[0;91m"
+b="\e[0;94m"
+b_bg="\e[0;104m${expand_bg}"
+r_bg="\e[0;101m${expand_bg}"
+g_bg="\e[0;102m${expand_bg}"
+g="\e[0;92m"
+white="\e[0;97m"
+u="\e[4m"
+re="\e[0m"
+#check for package manager
+var=$(ls /usr/bin | grep -E "^pacman|^apt|^zypper|^yum|^eopkg" | head -n 1)
+echo "found $var package manager"
 apt_update() {
-#check for apt-get package manager
- if [ -x "/usr/bin/apt-get" ];
- then
-   echo "" && echo "===== Update ======"
-   apt -y update
-   echo "" && echo "===== Dist-Upgrade ====="
-   apt -y dist-upgrade
-   echo "" && echo "===== Upgrade ======"
-   apt -y upgrade
-   echo "" && echo "===== Autoremove ======"
-   apt -y autoremove
-   echo "" && echo "===== Fix Dependancies ======"
-   apt -y install -f
- else
-   echo "'apt-get' package manager not found"
- fi
+	echo "" && echo "===== Update ======"
+	apt -y update
+	echo "" && echo "===== Dist-Upgrade ====="
+	apt -y dist-upgrade
+	echo "" && echo "===== Upgrade ======"
+	apt -y upgrade
+	echo "" && echo "===== Autoremove ======"
+	apt -y autoremove
+	echo "" && echo "===== Fix Dependancies ======"
+	apt -y install -f
 }
-
 yum_update() {
-#check for yum packager manager
- if [ -x "/usr/bin/yum" ];
- then
-   echo "" && echo "===== Update ======"
-   yum update -y 
-   echo "" && echo "===== upgrade ======"
-   yum upgrade -y
- else
-   echo "'yum' package manager not found"
- fi
+	echo "" && echo "===== Update ======"
+	yum update -y
+	echo "" && echo "===== upgrade ======"
+	yum upgrade -y
 }
-
 zypper_update() {
-#check for zypper packager manager
- if [ -x "/usr/bin/zypper" ];
- then
-   echo "" && echo "===== Starting update ====="
-   zypper --non-interactive update
- else
-   echo "'zypper' package manager not found"
- fi
+	echo "" && echo "===== Starting update ====="
+	zypper --non-interactive update
 }
-
 eopkg_update() {
-#check for eopkg packager manager
- if [ -x "/usr/bin/eopkg" ];
- then
-   echo "" && echo "===== Starting update ====="
-   eopkg upgrade -y
- else
-   echo "'eopkg' package manager not found"
- fi
+	echo "" && echo "===== Starting update ====="
+	eopkg upgrade -y
 }
-
 pacman_update() {
-#check for pacman packager manager
- if [ -x "/usr/bin/pacman" ];
- then
-   echo "" && echo "===== Starting update ====="
-   yes | pacman -Syu
- else
-   echo "'eopkg' package manager not found"
- fi
+	echo "" && echo "===== Starting update ====="
+	yes | pacman -Syu
 }
-do_update
+if [ $var == 'apt' 2>/dev/null ]; then
+	apt_update
+elif [ $var == 'yum' 2>/dev/null ]; then
+	yum_update
+elif [ $var == 'zypper' 2>/dev/null ]; then
+	zypper_update
+elif [ $var == 'pacman' 2>/dev/null ]; then
+	pacman_update
+elif [ -z $var ]; then
+	echo 'error, check package manager'
+fi
