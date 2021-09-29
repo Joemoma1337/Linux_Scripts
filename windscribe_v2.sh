a9158f4af6afb4a428d0c15d5e77ffcd29178293
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2021-09-29 13:59:37 -0400

    Update windscribe_v2.sh

--- windscribe_v2.sh
+++ windscribe_v2.sh
@@ -18,44 +18,35 @@ apt_install() {
         apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7
         echo "$===== Repository ====="
         echo 'deb https://repo.windscribe.com/ubuntu bionic main' | sudo tee /etc/apt/sources.list.d/windscribe-repo.list
-        echo "$===== update ====="
-        apt -y update
-        echo "$===== Install ====="
-        apt-get -y install windscribe-cli
-        echo "$===== Fix ====="
-        apt -y install -f
+        echo "$===== update =====" && apt -y update
+        echo "$===== Install =====" && apt-get -y install windscribe-cli
+        echo "$===== Fix =====" && apt -y install -f
         exit
     fi
 }
 yum_install() {
     if check_cmd yum && check_cmd yum-config-manager; then
-        echo "$===== Wget ====="
-        wget https://repo.windscribe.com/fedora/windscribe.repo -O /etc/yum.repos.d/windscribe.repo
-        echo "$===== Update ====="
-        yum -y update
-        echo "$===== Install ====="
-        yum -y install windscribe-cli
+        echo "$===== Wget =====" && wget https://repo.windscribe.com/fedora/windscribe.repo -O /etc/yum.repos.d/windscribe.repo
+        echo "$===== Update =====" && yum -y update
+        echo "$===== Install =====" && yum -y install windscribe-cli
         exit
     fi
 }
 pacman_install() {
     if check_cmd pacman; then
-        echo "Pacman detected, unable to install"
-        echo "check Windscribe support"
+        echo "Pacman detected, unable to install" && echo "check Windscribe support"
         exit
     fi
 }
 zypper_install() {
     if check_cmd zypper; then
-        echo "Zypper detected, unable to install"
-        echo "check Windscribe support"
+        echo "Zypper detected, unable to install" && echo "check Windscribe support"
         exit
     fi
 }
 eopkg_install() {
     if check_cmd eopkg; then
-        echo "" && echo "Eopkg detected, unable to install"
-        echo "" && echo "check Windscribe support"
+        echo "" && echo "Eopkg detected, unable to install" && echo "" && echo "check Windscribe support"
         exit
     fi
 }
