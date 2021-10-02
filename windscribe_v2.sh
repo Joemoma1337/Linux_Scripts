cbd2ac5e095f310322ba9c026b67f98684e91ea4
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2021-10-02 12:09:05 -0400

    Update windscribe_v2.sh

--- windscribe_v2.sh
+++ windscribe_v2.sh
@@ -8,55 +8,55 @@ if [ "$(id -u)" -ne 0 ]; then
         exit 1
     fi
 fi
+#check for package mangers
 check_cmd() {
     command -v "$1" 2> /dev/null
 }
- apt_install() {
+apt_install() {
     if check_cmd apt-get; then
-      wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-      apt -y install apt-transport-https
-      echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
-      echo "===== update =====" && apt -y update
-      echo "===== Install =====" && apt -y install sublime-text
-      echo "===== Fix-Broken =====" && apt -y install -f
-      exit
+        echo "===== Signing Key ====="
+        apt-key adv --keyserver keyserver.ubuntu.com --recv-key FDC247B7
+        echo "===== Repository ====="
+        echo 'deb https://repo.windscribe.com/ubuntu bionic main' | sudo tee /etc/apt/sources.list.d/windscribe-repo.list
+        echo "===== update =====" && apt -y update
+        echo "===== Install =====" && apt-get -y install windscribe-cli
+        echo "===== Fix =====" && apt -y install -f
+        exit
     fi
 }
 dnf_install() {
     if check_cmd dnf; then
-        echo "===== Import =====" && rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-        echo "===== Add-Repo =====" && dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-        echo "===== Install =====" && dnf install sublime-text
+        echo "===== Wget =====" && wget https://repo.windscribe.com/fedora/windscribe.repo -O /etc/yum.repos.d/windscribe.repo
+		echo "===== Add-epel =====" && dnf -y install epel-release
+        echo "===== Update =====" && dnf -y update
+        echo "===== Install =====" && dnf -y install windscribe-cli
         exit
     fi
 }
 yum_install() {
     if check_cmd yum && check_cmd yum-config-manager; then
-        echo "===== Import =====" && rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-        echo "===== Add-Repo =====" && yum-config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-        echo "===== Install =====" && yum -y install sublime-text
+        echo "===== Wget =====" && wget https://repo.windscribe.com/fedora/windscribe.repo -O /etc/yum.repos.d/windscribe.repo
+		echo "===== Add-epel =====" && yum -y install epel-release
+        echo "===== Update =====" && yum -y update
+        echo "===== Install =====" && yum -y install windscribe-cli
         exit
     fi
 }
 pacman_install() {
     if check_cmd pacman; then
-        echo "===== Curl ====="
-        curl -O https://download.sublimetext.com/sublimehq-pub.gpg && pacman-key --add sublimehq-pub.gpg && pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
-        if grep -q 'Server = https://download.sublimetext.com/arch/stable/x86_64' "/etc/pacman.conf";
-            then
-            echo "source already added"
-            else
-            echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | tee -a /etc/pacman.conf
-        fi
-        echo "===== Install =====" && yes | pacman -Syu sublime-text
+        echo "Pacman detected, unable to install" && echo "check Windscribe support"
         exit
     fi
 }
 zypper_install() {
     if check_cmd zypper; then
-        echo "===== Import =====" && rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-        echo "===== Add-Repo =====" && zypper addrepo -g -f https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-        echo "===== Install =====" && zypper --non-interactive install sublime-text
+        echo "Zypper detected, unable to install" && echo "check Windscribe support"
+        exit
+    fi
+}
+eopkg_install() {
+    if check_cmd eopkg; then
+        echo "" && echo "Eopkg detected, unable to install" && echo "" && echo "check Windscribe support"
         exit
     fi
 }
@@ -65,6 +65,7 @@ dnf_install
 yum_install
 pacman_install
 zypper_install
+eopkg_install
 # None of the known package managers (apt, yum, pacman, zypper) are available
 echo "Error: Couldn't identify the package manager"
 exit 1
