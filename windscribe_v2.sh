8149c658fd1c34d3421328861f756496062f26c6
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2021-10-02 11:58:49 -0400

    Update windscribe_v2.sh

--- windscribe_v2.sh
+++ windscribe_v2.sh
@@ -27,9 +27,18 @@ apt_install() {
 yum_install() {
     if check_cmd yum && check_cmd yum-config-manager; then
         echo "===== Wget =====" && wget https://repo.windscribe.com/fedora/windscribe.repo -O /etc/yum.repos.d/windscribe.repo
-	echo "===== Add-epel =====" && yum -y install epel-release
+		echo "===== Add-epel =====" && yum -y install epel-release
         echo "===== Update =====" && yum -y update
         echo "===== Install =====" && yum -y install windscribe-cli
+        exit
+    fi
+}
+dnf_install() {
+    if check_cmd yum && check_cmd yum-config-manager; then
+        echo "===== Wget =====" && wget https://repo.windscribe.com/fedora/windscribe.repo -O /etc/yum.repos.d/windscribe.repo
+		echo "===== Add-epel =====" && dnf -y install epel-release
+        echo "===== Update =====" && dnf -y update
+        echo "===== Install =====" && dnf -y install windscribe-cli
         exit
     fi
 }
