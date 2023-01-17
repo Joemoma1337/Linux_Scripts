c0fee6c14e384a8c49dd4e5b0e3aa4f013b64990
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2023-01-17 18:14:30 -0500

    Update windscribe_random.sh

--- testing/windscribe_random.sh
+++ testing/windscribe_random.sh
@@ -2,5 +2,5 @@
 array=("Boltzmann" "Vltava" "Jardin" "Wurstchen" "Grafton" "Duomo" "Tulip" "Vistula" "Ikea" "Lindenhof" "Custard" "Ghost" "Phooey" "Drift" "Marina Bay" "Han River" "station")
 
 size=${#array[@]}
-index=$RANDOM % $size
+index=$(( $RANDOM % $size ))
 windscribe-cli connect ${array[$index]}
