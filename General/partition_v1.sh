2b2975e58d9d473194336c536415bb5e99d33904
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2023-01-20 08:45:52 -0500

    Update partition_v1.sh

--- General/partition_v1.sh
+++ General/partition_v1.sh
@@ -8,17 +8,17 @@ if grep -q "$sub" <<< "$drive"; then
 	drive2=$(echo $drive | cut -d '/' -f 3)
 	mount=$(lsblk | grep $drive2 | awk '{print $7}')
 	umount $mount
-	sudo wipefs -a $drive'1'
-	echo ';' | sudo sfdisk $drive
+	wipefs -a $drive'1'
+	echo ';' | sfdisk $drive
 	if [[ "$format" == fat32 ]]; then
-		echo y | sudo mkfs.fat $drive'1'
-		sudo fatlabel $drive'1' $name
+		echo y | mkfs.fat $drive'1'
+		fatlabel $drive'1' $name
 	elif [[ "$format" == ext4 ]]; then
-		echo y | sudo mkfs.ext4 $drive'1'
-		sudo e2label $drive'1' $name
+		echo y | mkfs.ext4 $drive'1'
+		e2label $drive'1' $name
 	elif [[ "$format" == ntfs ]]; then
-		echo y | sudo mkfs.ntfs -f $drive'1'
-		sudo ntfslabel $drive'1' $name
+		echo y | mkfs.ntfs -f $drive'1'
+		ntfslabel $drive'1' $name
 	fi
 	echo ""&&echo "===== Partitioning Complete! ====="
 elif grep -vq "$sub" <<< "$var1"; then
