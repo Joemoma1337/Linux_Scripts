8cd7247de991da56d879dd61f66cdd62086f9354
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2023-06-24 17:18:16 -0400

    Update ventoy_pikvm.sh

--- General/ventoy_pikvm.sh
+++ General/ventoy_pikvm.sh
@@ -10,7 +10,12 @@ read count
 dd if=/dev/zero of=ventoy.img bs=1024M count=$count status=progress
 echo "===== Wget ====="
 #download ventoy software
-wget https://github.com/ventoy/Ventoy/releases/download/v1.0.51/ventoy-1.0.51-linux.tar.gz
+if [ -f "ventoy-1.0.51-linux.tar.gz" ]; then
+	continue 
+else 
+	wget https://github.com/ventoy/Ventoy/releases/download/v1.0.51/ventoy-1.0.51-linux.tar.gz
+fi
+
 tar zxvf ventoy-1.0.51-linux.tar.gz
 echo "===== losetup ====="
 #mount venoty.img to loop device
@@ -27,7 +32,7 @@ sudo mkdir /media/ventoy
 sudo mount "/dev/$ventoy_loop"p1 /media/ventoy
 echo "===== copy ====="
 #copy images
-echo "Full path to directory you wish to transfer ALL files from (ex: /home/ubuntu/Downloads)"
+echo "Full path to directory you wish to trasnsfer ALL files from (ex: /home/ubuntu/Downloads)"
 read iso_dir
 sudo cp -v $iso_dir/* /media/ventoy
 
@@ -40,10 +45,10 @@ echo "===== changing dir to rw ====="
 ssh root@$PiKVMIP "mount -o remount,rw /var/lib/kvmd/msd"
 echo "===== transferring ventoy.img ====="
 scp ventoy.img root@$PiKVMIP:/var/lib/kvmd/msd
-echo "===== create complete file ====="
-ssh root@$PiKVMIP "touch /var/lib/kvmd/msd/.__ventoy.img.complete"
-echo "===== changing dir to ro ====="
-ssh root@$PiKVMIP "mount -o remount,ro /var/lib/kvmd/msd"
+echo "===== create complete file && changing dir to ro ====="
+ssh root@$PiKVMIP "touch /var/lib/kvmd/msd/.__ventoy.img.complete && mount -o remount,ro /var/lib/kvmd/msd"
+#echo "===== changing dir to ro ====="
+#ssh root@$PiKVMIP "mount -o remount,ro /var/lib/kvmd/msd"
 
 echo "===== cleanup ====="
 sudo umount /media/ventoy
