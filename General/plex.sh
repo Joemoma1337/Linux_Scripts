02e7db1c1a27609a7119af59a7f458f09fc80d96
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2023-10-01 07:32:43 -0400

    Update plex.sh

--- General/plex.sh
+++ General/plex.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
+currentinstalled=$(dpkg -s plexmediaserver|grep Version|cut -d ' ' -f2)
+plexlatest=$(curl "https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu"| cut -d '/' -f5)
 version=$(curl "https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu"| cut -d '/' -f 7 | cut -d '"' -f 1)
-# Download the latest Plex Media Server package for Ubuntu 64-bit
-wget -O $version "https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu"
-# Install the package
-sudo dpkg -i $version
-# Clean up the downloaded package
-#rm ~/plexmediaserver.deb
+
+if [ -z "$currentinstalled" ]; then
+  # currentinstalled is empty, Download the latest Plex Media Server package for Ubuntu 64-bit
+        wget -O $version "https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu"
+        # Install the package
+        sudo dpkg -i $version
+elif [[ "$currentinstalled" > "$plexlatest" ]]; then
+  # currentinstalled is greater than plexlatest, do nothing
+  :
+elif [[ "$currentinstalled" < "$plexlatest" ]]; then
+  # currentinstalled is less than plexlatest, Download the latest Plex Media Server package for Ubuntu 64-bit
+        wget -O $version "https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu"
+        # Install the package
+        sudo dpkg -i $version
+else
+  # currentinstalled is equal to plexlatest, do nothing
+         echo "===== Already up to date ====="
+fi
