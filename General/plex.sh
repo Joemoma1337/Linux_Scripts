6a5c711ba1d49936fc794e6cd45a2304708d0573
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2024-04-21 15:03:17 -0400

    Update plex.sh

--- General/plex.sh
+++ General/plex.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 currentinstalled=$(dpkg -s plexmediaserver|grep Version|cut -d ' ' -f2)
-plexlatest=$(curl "https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu"| cut -d '/' -f5)
-version=$(curl "https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu"| cut -d '/' -f 7 | cut -d '"' -f 1)
+#plexlatestV=$(curl "https://plex.tv/downloads/details/1?build=linux-ubuntu-x86_64&channel=16&distro=ubuntu"|grep 'Release id'|cut -d '"' -f4)
+version=$(curl "https://plex.tv/downloads/details/1?build=linux-ubuntu-x86_64&channel=16&distro=ubuntu"|grep 'Release id'|cut -d '"' -f4)
+latesturl=$(curl "https://plex.tv/downloads/details/1?build=linux-ubuntu-x86_64&channel=16&distro=ubuntu"|grep 'url='|cut -d '"' -f18)
 
 if [ -z "$currentinstalled" ]; then
   # currentinstalled is empty, Download the latest Plex Media Server package for Ubuntu 64-bit
-        wget -O $version "https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu"
+        wget -O "plexmediaserver_"$version"_amd64.deb" "$latesturl"
         # Install the package
         sudo dpkg -i $version
-elif [[ "$currentinstalled" > "$plexlatest" ]]; then
-  # currentinstalled is greater than plexlatest, do nothing
+elif [[ "$currentinstalled" > "$version" ]]; then
+  # currentinstalled is greater than version, do nothing
   :
-elif [[ "$currentinstalled" < "$plexlatest" ]]; then
-  # currentinstalled is less than plexlatest, Download the latest Plex Media Server package for Ubuntu 64-bit
-        wget -O $version "https://plex.tv/downloads/latest/1?channel=16&build=linux-ubuntu-x86_64&distro=ubuntu"
+elif [[ "$currentinstalled" < "$version" ]]; then
+  # currentinstalled is less than version, Download the latest Plex Media Server package for Ubuntu 64-bit
+        wget -O $version "$latesturl"
         # Install the package
         sudo dpkg -i $version
+else
+  # currentinstalled is equal to plexlatest, do nothing
+         echo "===== Already up to date ====="
+fi
