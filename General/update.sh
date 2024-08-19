345c8ffb15d63cac418e779ba52ebc10c4fe0e3c
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2024-08-19 06:31:59 -0400

    Update update.sh

--- General/update.sh
+++ General/update.sh
@@ -6,10 +6,10 @@ if [ -f /etc/redhat-release ]; then
         echo "" && echo "===== upgrade ======" && yum upgrade -y
 elif [ -f /etc/debian_version ]; then
     # Update for Debian-based distributions
-        echo "" && echo "===== Update ======" && apt -y update
-        echo "" && echo "===== Upgrade ======" && apt -y upgrade
-        echo "" && echo "===== Fix Dependancies ======" && apt -y install -f
-        echo "" && echo "===== Autoremove ======" && apt -y autoremove
+        echo "" && echo "===== Update ======" && apt update -y
+        echo "" && echo "===== Upgrade ======" && apt upgrade -y
+        echo "" && echo "===== Fix Dependancies ======" && apt install -y  -f
+        echo "" && echo "===== Autoremove ======" && apt autoremove -y
 elif [ -f /etc/arch-release ]; then
     # Update for Arch Linux-based distributions
 		echo "" && echo "===== Starting update =====" && yes | pacman -Syu
