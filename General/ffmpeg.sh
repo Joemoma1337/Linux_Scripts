fd945209eb78b7749fe70b931cd83e4d726be8ea
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2024-08-19 06:30:41 -0400

    Update ffmpeg.sh

--- General/ffmpeg.sh
+++ General/ffmpeg.sh
@@ -1,6 +1,6 @@
 #CPU
-for i in $(ls *.mkv); do ffmpeg -i "$i" -map 0 ../0.Backup/"2-${i}"; done ; cd ../S01
+for i in $(ls *.mkv); do ffmpeg -i "$i" -map 0 ../Backup/"2-${i}"; done ; cd ../S01
 #GPU
-for a in *.{mkv,mp4,m4v,avi}; do ffmpeg -hwaccel_output_format cuda -c:v h264_cuvid -i "$a" -c:a copy -c:v h264_nvenc -map 0 ../0.Backup/"${a}"; done
+for a in *.{mkv,mp4,m4v,avi}; do ffmpeg -hwaccel_output_format cuda -c:v h264_cuvid -i "$a" -c:a copy -c:v h264_nvenc -map 0 ../Backup/"${a}"; done
 #GPU encode entire directory
-for i in $(ls /mnt/8TB/Movies/|grep -Ev '0.Backup|1.Movies.list');do cd /mnt/8TB/Movies/$i && for a in *.{mkv,mp4,m4v,avi}; do ffmpeg -hwaccel_output_format cuda -c:v h264_cuvid -i "$a" -c:a copy -c:v h264_nvenc -map 0 ../0.Backup/"${a}"; done; done
+for i in $(ls /Path/To/Source/|grep -Ev 'exclude1|exclude2');do cd /Path/To/Source/$i && for a in *.{mkv,mp4,m4v,avi}; do ffmpeg -hwaccel_output_format cuda -c:v h264_cuvid -i "$a" -c:a copy -c:v h264_nvenc -map 0 ../Backup/"${a}"; done; done
