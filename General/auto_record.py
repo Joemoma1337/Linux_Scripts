e93acc46cdf4edc48bccc3f660749d1bcde2da49
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2024-08-20 10:28:08 -0400

    Update auto_record.py

--- General/auto_record.py
+++ General/auto_record.py
@@ -19,7 +19,7 @@ def bring_window_to_foreground(window_title):
     except Exception as e:
         print(f"Error bringing window to foreground: {type(e).__name__} - {e}")
 
-def check_image_match(image_path, region, confidence=0.6):
+def check_image_match(image_path, region, confidence=0.9):
     """Check if the image is on the screen within the specified region."""
     try:
         return pyautogui.locateOnScreen(image_path, region=region, confidence=confidence)
@@ -34,17 +34,15 @@ def main():
     """Main function to run the automation script."""
     l_mon = get_monitors()[0]
     region = (l_mon.x + 375, l_mon.y + 210, 50, 50)  # Region within the specified box
-    #region1 = (l_mon.x + 375, l_mon.y + 210, 50, 50)  # Region within the specified box
-    print(f"Region for searching 'start.png': {region}")
+    print(f"Region for searching 'TCM_start.png': {region}")
 
     try:
         while True:
-            # Search for start.png
-            found1 = check_for_image('start.png', region)
+            # Search for TCM_start.png
+            found1 = check_for_image('TCM_start.png', region)
             
             if found1:
-                # At beginning of video: Start recording
-                print("start.png found.")
+                print("TCM_start.png found.")
                 pyautogui.click(l_mon.x + 1125, l_mon.y + 540)
                 pyautogui.sleep(0.2)
                 pyautogui.click(l_mon.x + 1125, l_mon.y + 540)
@@ -55,6 +53,7 @@ def main():
                 pyautogui.sleep(0.75)
                 pyautogui.click(l_mon.x + 1125, l_mon.y + 540)
                 pyautogui.sleep(0.1)
+                # At beginning of video: Start recording
                 bring_window_to_foreground("obs")
                 pyautogui.sleep(0.1)
                 pyautogui.moveTo(2800, 720)
@@ -66,39 +65,39 @@ def main():
                 # Loop until one of the target images is found
                 while True:
                     # Search for all target images
-                    found2 = check_for_image('stop.png', region)
-                    found3 = check_for_image('challenge.png', region)
-                    found4 = check_for_image('quiz.png', region)
+                    found2 = check_for_image('TCM_stop.png', region)
+                    found3 = check_for_image('TCM_challenge.png', region)
+                    found4 = check_for_image('TCM_quiz.png', region)
 
                     if found2:
-                        print("stop.png found.")
+                        print("TCM_stop.png found. Stopped recording")
                         bring_window_to_foreground("obs")
                         pyautogui.sleep(0.1)
                         pyautogui.hotkey('F9')
                         pyautogui.sleep(2)
-                        # Actions for stop.png
+                        # Actions for TCM_stop.png
                         break  # Exit the loop after handling the found image
                         
                     if found3:
-                        print("challenge.png found.")
+                        print("TCM_challenge.png found. Stopped recording")
                         bring_window_to_foreground("obs")
                         pyautogui.sleep(0.1)
                         pyautogui.hotkey('F9')
                         pyautogui.sleep(2)
                         pyautogui.click(l_mon.x + 1650, l_mon.y + 155)
                         pyautogui.sleep(2)
-                        # Actions for challenge.png
+                        # Actions for TCM_challenge.png
                         break
 
                     if found4:
-                        print("quiz.png found.")
+                        print("TCM_quiz.png found. Stopped recording")
                         bring_window_to_foreground("obs")
                         pyautogui.sleep(0.1)
                         pyautogui.hotkey('F9')
                         pyautogui.sleep(2)
                         pyautogui.click(l_mon.x + 1650, l_mon.y + 155)
                         pyautogui.sleep(2)
-                        # Actions for quiz.png
+                        # Actions for TCM_quiz.png
                         break
 
     except KeyboardInterrupt:
