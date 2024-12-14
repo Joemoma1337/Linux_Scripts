dc7360517ff703d17d391a553ffe7aada2c9f881
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2024-12-14 08:41:35 -0500

    Update 4kDownload_reset.sh

--- General/4kDownload_reset.sh
+++ General/4kDownload_reset.sh
@@ -1,4 +1,5 @@
 pkill -f 4kvideodownloaderplus
+cp "/home/USER/Videos/4K Video Downloader+"/* /home/standard/Videos/backup/
 sudo apt -y remove 4kvideodownloaderplus
-find ./ -path './Downloads' -prune -o -iname '4k*video*' -print0 | while IFS= read -r -d '' i; do rm -rf "$i"; done
-sudo dpkg -i ./Downloads/4kvideodownloaderplus_1.9.0-1_amd64.deb
+find / \( -path '/home/USER/Downloads' -o -path '/mnt' \) -prune -o -iname '4k*video*' -print0 2>/dev/null | while IFS= read -r -d '' i; do rm -rf "$i"; done
+sudo dpkg -i /home/USER/Downloads/4kdl.deb
