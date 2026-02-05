062682b273f30b0c95ca1caa07eb7a8156457543
Joemoma1337 <39353000+Joemoma1337@users.noreply.github.com>
2026-02-05 02:22:08 -0500

    Update or.sh

--- basic_scripts/or.sh
+++ basic_scripts/or.sh
@@ -3,4 +3,4 @@ Description
 The OR operator (represented as ||) allows a script to proceed if at least one of multiple conditions is met. Unlike the AND operator, which requires everything to be perfect, the OR operator returns True if any single part of the expression is true.
 
 Think of it like a guest list: you can get into the party if you have an invitation OR if you are on the VIP list.
-![alt text](https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcR5fa93Vc0b8H694ugTj7x_9eRrCAIROkkRjOZH1tc1a2udu7ah5SzVI0YgjDmRLEikbnTRi02ETauyGwNNIeczXIqBMf6P8PYOuqYFoUaLti33Dao "Logic Gates with Truth Tables")
+"Logic Gates with Truth Tables": https://encrypted-tbn0.gstatic.com/licensed-image?q=tbn:ANd9GcR5fa93Vc0b8H694ugTj7x_9eRrCAIROkkRjOZH1tc1a2udu7ah5SzVI0YgjDmRLEikbnTRi02ETauyGwNNIeczXIqBMf6P8PYOuqYFoUaLti33Dao
